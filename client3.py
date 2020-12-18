fdd064b73f4c559a6f13cd696644155068c52753
j7olivar <j7olivar@ucsd.edu>
2020-12-17 17:51:39 -0800

    Update client3.py

--- client3.py
+++ client3.py
@@ -42,7 +42,8 @@ def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
 	""" ------------- Update this function ------------- """
 	""" Also create some unit tests for this function in client_test.py """
-	if(price_b == 0) return
+	if(price_b == 0):
+		 return
 	return price_a/price_b
 
 # Main
